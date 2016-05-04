0acd0da50df77a553a7ff461fac88b743ab36b01
duplyk.a <dlit@yandex.ru>
2016-05-04 11:38:55 +0300

    Added MappedSuperClass annotation.

--- src/main/java/org/openbox/sf5/model/AbstractDbEntity.java
+++ src/main/java/org/openbox/sf5/model/AbstractDbEntity.java
@@ -1,7 +1,14 @@
 package org.openbox.sf5.model;
 
+import java.io.Serializable;
+
+import javax.persistence.MappedSuperclass;
+
 // This class should be used in Hibernate entities that are stored in database.
-public abstract class AbstractDbEntity {
+@MappedSuperclass
+public class AbstractDbEntity implements Serializable {
+
+	private static final long serialVersionUID = -4019144518870996041L;
 
 	@Override
 	public boolean equals(Object obj) {
