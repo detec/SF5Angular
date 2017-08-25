ec61486443d99e907e89128f601f3b9a6cdfe585
WINMAIN\Andrii <andrii.duplyk@nitka.com>
2017-08-25 09:53:38 +0300

    Refactoring

--- src/main/java/org/openbox/sf5/json/endpoints/UsersService.java
+++ src/main/java/org/openbox/sf5/json/endpoints/UsersService.java
@@ -2,6 +2,9 @@ package org.openbox.sf5.json.endpoints;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.ws.rs.core.MediaType;
 
 import org.openbox.sf5.common.SF5SecurityContext;
 import org.openbox.sf5.json.exceptions.NotAuthenticatedException;
@@ -12,10 +15,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -27,7 +33,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @RestController
 @EnableWebMvc
-@RequestMapping(value = "${jaxrs.path}/users/")
+@RequestMapping(value = "${jaxrs.path}/users/", produces = MediaType.APPLICATION_JSON)
 public class UsersService {
 
 	private static final String CONSTANT_COULDNT_GET_USER = "Couldn't get currently authenticated user!";
@@ -39,7 +45,7 @@ public class UsersService {
 	private SF5SecurityContext securityContext;
 
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
 	ResponseEntity<List<Users>> getAllUsers() throws NotAuthenticatedException {
 		getVerifyAuthenticatedUser();
 
@@ -49,7 +55,7 @@ public class UsersService {
 	}
 
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@RequestMapping(value = "{userId}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "{userId}")
 	ResponseEntity<Users> deleteUser(@PathVariable("userId") long userId) {
 		Users user = null;
 		try {
@@ -80,7 +86,7 @@ public class UsersService {
 	// https://docs.spring.io/spring-security/site/docs/3.0.x/reference/el-access.html
 	// We allow only for admin and enabled user.
 	@PreAuthorize("hasRole('ROLE_ADMIN') or (#login  == authentication.name)")
-	@RequestMapping(value = "filter/username/{login}", method = RequestMethod.GET)
+    @GetMapping("filter/username/{login}")
 	ResponseEntity<Users> getUserByLogin(@PathVariable("login") String login) {
 		Users retUser = null;
 		try {
@@ -89,18 +95,14 @@ public class UsersService {
 			throw new IllegalStateException("Error getting user from database!", e);
 		}
 
-		if (retUser == null) {
-			throw new IllegalArgumentException("No user found in database for login: " + login);
-		}
-
-		return new ResponseEntity<>(retUser, HttpStatus.OK);
-
+        return Optional.ofNullable(retUser).map(user -> new ResponseEntity<>(user, HttpStatus.OK)).orElseThrow(
+                () -> new IllegalArgumentException(String.join("", "No user found in database for login: ", login)));
 	}
 
 	// anonymous access is needed.
 	@PreAuthorize("hasRole('ROLE_ADMIN')") // only admin can save changed users
 											// with REST.
-	@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
 	ResponseEntity<Users> saveUser(@RequestBody Users user) {
 		// check if such user exists.
 
@@ -131,23 +133,19 @@ public class UsersService {
 
 	@PreAuthorize("hasRole('ROLE_ADMIN')") // only admin can save changed users
 	// with REST.
-	@RequestMapping(value = "{userId}", method = RequestMethod.PUT)
+    @PutMapping("{userId}")
 	ResponseEntity<Users> updateUser(@RequestBody Users user) {
 		return saveUser(user);
 	}
 
-	@RequestMapping(value = "exists/username/{login}", method = RequestMethod.GET)
+    @GetMapping("exists/username/{login}")
 	ResponseEntity<Boolean> ifSuchLoginExists(@PathVariable("login") String login) {
-		Boolean result = usersJsonizer.checkIfUsernameExists(login);
-		if (!result) {
-			return new ResponseEntity<>(result, HttpStatus.NO_CONTENT);
-		}
-
-		return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+        return usersJsonizer.checkIfUsernameExists(login) ? new ResponseEntity<>(true, HttpStatus.ACCEPTED)
+                : new ResponseEntity<>(false, HttpStatus.NO_CONTENT);
 	}
 
 	@PreAuthorize("hasRole('ROLE_USER')")
-	@RequestMapping(value = "currentuser", method = RequestMethod.GET)
+    @GetMapping("currentuser")
 	ResponseEntity<List<Users>> getCurrentlyAuthenticatedUser() throws NotAuthenticatedException {
 		Users currentUser = getVerifyAuthenticatedUser();
 
